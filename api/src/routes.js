c167496a46918f7637a16d18a4bf47bde7111aed
khaled <lga.sig.khaled@gmail.com>
2021-11-30 00:02:29 +0100

    config swagger

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const router = express.Router();
 router.use(express.json());
 
-
+router.use('/api', swaggerUi.serve);
+router.get('/api', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
 
