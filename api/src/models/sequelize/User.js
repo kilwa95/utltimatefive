043bbeeb77aa0642c2aa54d0cb43a9a9b828b555
khaled <lga.sig.khaled@gmail.com>
2021-11-30 21:49:42 +0100

    model user

---
+++ api/src/models/sequelize/User.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const connection = require('../../config/sequelize');
+
+class User extends Model {}
+
+User.init(
+    {
+        firstName:{ type: DataTypes.STRING,allowNull:false},
+        lastName:{ type: DataTypes.STRING,allowNull:false},
+        email:{ type: DataTypes.STRING,allowNull:false,unique:true,validate:{isEmail:true}},
+        password:{ type: DataTypes.STRING,allowNull:false,unique:true},
+        enable:{ type: DataTypes.BOOLEAN,defaultValue:true},
+        roles:{ type: DataTypes.ARRAY,allowNull:false},
+        birthday:{ type: DataTypes.DATEONLY,allowNull:true},
+        status:{ type: DataTypes.STRING,allowNull:false},
+    },
+    {
+        sequelize: connection,
+        modelName: 'User',
+    }
+)
+
+const haschPassword = async (user) => {
+    user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
+}
+User.addHook('beforeCreate',haschPassword)
+User.addHook('beforeUpdate', haschPassword);
+
+
+User.sync({
+	alter: true
+})
+
+module.exports = User;
