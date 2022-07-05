68fc02d095815a59a145403229b0e05719445179
khaled <lga.sig.khaled@gmail.com>
2022-07-05 12:06:37 +0200

    tteste

--- api/src/__tests__/Users.test.js
+++ api/src/__tests__/Users.test.js
@@ -1,5 +1,16 @@
 const request = require('supertest')
 const app = require('../app')
+const Sequelize = require('sequelize')
+
+let connection
+beforeAll(() => {
+  connection = new Sequelize(process.env.DATABASE_URL, {})
+  connection.authenticate().then((_) => console.log('pg connected'))
+})
+
+afterAll(async () => {
+  await connection.close()
+})
 
 describe('GET /user', function () {
   it('responds with json', function (done) {
