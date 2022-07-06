0b2c7c2db16ad572e181da87cb0a43f7660f90a4
khaled <lga.sig.khaled@gmail.com>
2022-07-06 22:38:54 +0200

    jest

--- api/src/__tests__/Users.test.js
+++ api/src/__tests__/Users.test.js
@@ -2,22 +2,6 @@ const request = require('supertest')
 const app = require('../app')
 const Sequelize = require('sequelize')
 
-let connection
-beforeAll(() => {
-  connection = new Sequelize(process.env.DATABASE_URL, {})
-  connection.authenticate().then((_) => console.log('pg connected'))
-})
-
-afterAll(async () => {
-  await connection.close()
-})
-
-describe('GET /user', function () {
-  it('responds with json', function (done) {
-    request(app)
-      .get('/users')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
-  })
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
 })
