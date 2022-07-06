0aef778fe71a17a208a2b3a1253b6a7db39547fe
khaled <lga.sig.khaled@gmail.com>
2022-07-06 22:56:06 +0200

    teste

--- api/src/__tests__/Users.test.js
+++ api/src/__tests__/Users.test.js
@@ -1,6 +1,6 @@
-const request = require('supertest')
-const app = require('../app')
-const Sequelize = require('sequelize')
+function sum(a, b) {
+  return a + b
+}
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
